78a904c1a882f07e9232da2860b647cb1736babe
andrei <andrei.malosti06@gmail.com>
2026-03-09 15:13:42 -0300

    feat: create user and task services

---
+++ src/main/java/com/taskmanager/domain/service/TaskService.java
@@ -0,0 +1,56 @@
+package com.taskmanager.domain.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.taskmanager.api.dto.TaskRequestDTO;
+import com.taskmanager.api.dto.TaskResponseDTO;
+import com.taskmanager.api.mapper.TaskMapper;
+import com.taskmanager.domain.entity.Task;
+import com.taskmanager.domain.exception.TaskNotFoundException;
+import com.taskmanager.domain.repository.TaskRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+	private final TaskRepository repository;
+	private final TaskMapper mapper;
+	
+	public List<TaskResponseDTO> findAll(){
+		return repository.findAll().stream().map(mapper::toResponseDTO).toList();
+	}
+	
+	public TaskResponseDTO findById(Long id) {
+		return mapper.toResponseDTO(findTaskByIdAux(id));
+	}
+	
+	@Transactional
+	public TaskResponseDTO create(TaskRequestDTO dto) {
+		Task task = mapper.toEntity(dto);
+		task = repository.save(task);
+		return mapper.toResponseDTO(task);
+	}
+	
+	@Transactional
+	public TaskResponseDTO update(TaskRequestDTO dto, Long id) {
+		Task task = findTaskByIdAux(id);
+		mapper.updateTaskFromDTO(task, dto);
+		task = repository.save(task);
+		return mapper.toResponseDTO(task);
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		Task task = findTaskByIdAux(id);
+		repository.delete(task);
+	}
+	
+	private Task findTaskByIdAux(Long id) {
+		return repository.findById(id).orElseThrow(() -> new TaskNotFoundException(String.format("Task with id %d not found", id)));
+	}
+}
