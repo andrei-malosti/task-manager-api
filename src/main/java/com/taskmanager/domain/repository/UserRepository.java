59e8e0ff59230cb4fa25349100c00961957d90cf
andrei <andrei.malosti06@gmail.com>
2026-03-09 13:25:50 -0300

    feat: add JPA Repositories

---
+++ src/main/java/com/taskmanager/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.taskmanager.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.taskmanager.domain.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
