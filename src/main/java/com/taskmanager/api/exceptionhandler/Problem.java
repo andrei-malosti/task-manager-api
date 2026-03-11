1f861f783233bdfd82f7be056df244972602bc6a
andrei <andrei.malosti06@gmail.com>
2026-03-11 15:19:42 -0300

    feat: implement custom handler for TypeMismatch and HttpMessageNotReadable

--- src/main/java/com/taskmanager/api/exceptionhandler/Problem.java
+++ src/main/java/com/taskmanager/api/exceptionhandler/Problem.java
@@ -3,9 +3,13 @@ package com.taskmanager.api.exceptionhandler;
 import java.time.OffsetDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.Builder;
 import lombok.Getter;
 
+@JsonInclude(Include.NON_NULL)
 @Getter
 @Builder
 public class Problem {
